2dc9d9b4b78ced43146d71de008b0f4183e5729d
NICOLAS <NICOLAS@NICOLAS-PC>
2015-06-21 15:11:59 -0300

    versao 1.2

--- src/engenhariasw/A.java
+++ src/engenhariasw/A.java
@@ -22,6 +22,16 @@ public class A {
     public void setA3(String A3) {
         this.A3 = A3;
     }
+    
+    private String Aprinc4;
+
+    public String getAprinc4() {
+        return Aprinc4;
+    }
+
+    public void setAprinc4(String Aprinc4) {
+        this.Aprinc4 = Aprinc4;
+    }
 
     private float A2;
     
